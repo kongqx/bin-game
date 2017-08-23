8f1f5bd653740a1f5a12706c8dfe8b2691d6ad31
nixawk <hap.ddup@gmail.com>
2017-08-22 23:45:29 -0400

    operator ++

--- operator_inc.c
+++ operator_inc.c
@@ -24,7 +24,7 @@ main()
 |           0x561c6a7b9661      4889e5         mov rbp, rsp
 |           0x561c6a7b9664      897dfc         mov dword [local_4h], edi    # PARAMETER: int x;
 |           0x561c6a7b9667      8b45fc         mov eax, dword [local_4h]
-|           0x561c6a7b966a      8d5001         lea edx, [rax + 1]      ; 1  # ****
+|           0x561c6a7b966a      8d5001         lea edx, [rax + 1]      ; 1  # !!! dr edx = 0x00000003
 |           0x561c6a7b966d      8955fc         mov dword [local_4h], edx
 |           0x561c6a7b9670      5d             pop rbp
 \           0x561c6a7b9671      c3             ret
